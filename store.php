6724a5a3a358dc98767a52103eca00a5d7a6386a
Ayuka-Okada <a.snow.rsc2@gmail.com>
2020-06-24 15:58:57 +0900

    add comments

--- store.php
+++ store.php
@@ -6,10 +6,12 @@ require_once('Models/Task.php');
 // データの受け取り
 $title = $_POST['title'];
 $contents = $_POST['contents'];
+// 作成した関数　組み込み関数
 $currentTime = date("Y/m/d H:i:s");
 
 // DBへのデータ保存
 $task = new Task();
+// 自作関数
 $task->create([$title, $contents, $currentTime]);
 
 // リダイレクト
