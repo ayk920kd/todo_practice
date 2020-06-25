77f9d4e36e88910495b33d2c30ac3f86372ea40b
Ayuka-Okada <a.snow.rsc2@gmail.com>
2020-06-25 17:06:20 +0900

    write down Edit function

--- index.php
+++ index.php
@@ -58,26 +58,26 @@
 
         <div class="row p-3">
             <?php foreach($tasks as $task) : ?>
-            <div class="col-sm-6 col-md-4 col-lg-3 py-3 py-3">
-                <div class="card">
-                    <img src="https://picsum.photos/200" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title"><?php echo h($task['title']); ?></h5>
-                        <p class="card-text">
-                            <?php echo h($task['contents']); ?>
-                        </p>
-                        <div class="text-right d-flex justify-content-end">
-                            <!-- * href内を変更する -->
-                            <a href="edit.php" class="btn text-success">EDIT</a>
-                            <form action="delete.php" method="post">
-                                <!-- * valueの中にtaskのidが入るようにする -->
-                                <input type="hidden" name="id" value="<?php echo h($task['id']); ?>">
-                                <button type="submit" class="btn text-danger">DELETE</button>
-                            </form>
+                <div class="col-sm-6 col-md-4 col-lg-3 py-3 py-3">
+                    <div class="card">
+                        <img src="https://picsum.photos/200" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo h($task['title']); ?></h5>
+                            <p class="card-text">
+                                <?php echo h($task['contents']); ?>
+                            </p>
+                            <div class="text-right d-flex justify-content-end">
+                                <!-- * href内を変更する -->
+                                <a href="edit.php?id=<?php echo h($task['id']); ?>" class="btn text-success">EDIT</a>
+                                <form action="delete.php" method="post">
+                                    <!-- * valueの中にtaskのidが入るようにする -->
+                                    <input type="hidden" name="id" value="<?php echo h($task['id']); ?>">
+                                    <button type="submit" class="btn text-danger">DELETE</button>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
             <?php endforeach; ?>
         </div>
     </div>
